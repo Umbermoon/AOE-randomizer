2d4a44a606a4413a50fd580b0200ad27075ca09c
umbermoon <aschwartzg@gmail.com>
2019-06-25 21:45:10 -0400

    Fix for IooB Exception during many generations without repeats

--- src/aoer/backend/CivRandomizer.java
+++ src/aoer/backend/CivRandomizer.java
@@ -8,13 +8,19 @@ public class CivRandomizer {
 	public static void randomizeCivs(Player[] pList, CivMap cMap, boolean allowedRepeats) {
 		Random rand = new Random();
 		Civs[] enabledCivs = cMap.getEnabledCivs();
+		Civs[] removedCivs = new Civs[pList.length];
 		int randomIndex = 0;
+		int iter = 0;
 		for(Player p : pList) {
 			randomIndex = rand.nextInt(enabledCivs.length);
 			p.setAssignedCiv(enabledCivs[randomIndex]);
-			if(!allowedRepeats)
+			if(!allowedRepeats) {
+				removedCivs[iter] = enabledCivs[randomIndex];
 				enabledCivs = removeCiv(enabledCivs, enabledCivs[randomIndex]);
+			}
+			iter++;
 		}
+		enabledCivs = addCivs(enabledCivs, removedCivs);
 	}
 	
 	private static Civs[] removeCiv(Civs[] list, Civs removeMe) {
@@ -27,6 +33,17 @@ public class CivRandomizer {
 			}	
 		}
 		return list;
+	}
+	
+	private static Civs[] addCivs(Civs[] list1, Civs[] list2) {
+		Civs[] combined = new Civs[list1.length + list2.length]; 
+		for(int i = 0; i < list1.length; i++) {
+			combined[i] = list1[i];
+		}
+		for(int i = list1.length; i < list1.length + list2.length; i++) {
+			combined[i] = list2[i - list1.length];
+		}
+		return combined;
 	}
 	
 }
